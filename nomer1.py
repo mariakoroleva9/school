3b5337230051fd47544f68b62317bbd0f774f81e
masia <masia.demina@yandex.ru>
2024-03-05 02:04:57 +0300

    zx

--- nomer1.py
+++ nomer1.py
@@ -8,11 +8,15 @@ students=[]
 f=open('students.csv', encoding = 'utf8')
 for i in range(501):
     students.append(Studentik())
-    s = f.readline().split()
-    students[i].fio=s[1]
-    students[i].id=s[0]
-    students[i].score = s[4]
+    s = f.readline().split(',')
+    if s[4] == None:
+        s[4] = -10
+    students[i].id = s[0]
+    students[i].fio = s[1]
     students[i].clas = s[3]
+    students[i].score = s[4]
+
 
     if 'Хадаров Владимир' in students[i].fio:
         print('Ты получил:', students[i].score, 'за проект -', students[i].id)
+        break
